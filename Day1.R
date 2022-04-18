672c830c9b101646dc82c7028b2451c5afd95f08
dlwodbeld <98381537+dlwodbeld@users.noreply.github.com>
2022-04-18 11:06:29 +0900

    Update Day1.R

--- Day1.R
+++ Day1.R
@@ -11,3 +11,39 @@ search()# 현재 로드된 페키지 확인인
 remove.packages("stringr")
 
 
+data()
+
+#변수
+
+var1 <- 0
+var1
+var1 <- 1
+var1
+
+goods.code <- 'a001'#문자값저장
+goods.name <- '냉장고'
+
+age <- 35
+name <- "홍길동"
+age
+name
+
+
+age <- 35
+names <- c("홍길동","이순신","유관순") #컴바인
+age
+names
+
+int <- 20
+int
+ 
+string <- "홍길동"
+
+boolean <- TRUE
+boolean
+
+sum(10,20,30)
+
+sum(10,20,30,NA)
+
+sum(10,20,30,na.rm=TRUE)
